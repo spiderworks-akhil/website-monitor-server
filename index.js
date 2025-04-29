2c93fa25ec08049c52e0daacea54db7478a28426
Roshan-Sw <roshan.sam@spiderworks.in>
2025-04-29 12:14:43 +0530

    change cors

--- index.js
+++ index.js
@@ -17,21 +17,15 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://website-monitor-client.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-// app.use(
-//   cors({
-//     origin: "https://website-monitor-client.vercel.app",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
 
 app.use(express.json());
 app.use(cookieParser());
