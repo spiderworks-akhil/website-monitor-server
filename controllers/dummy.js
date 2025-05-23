adbef3cd638e125be851995d1fde2c14e411155d
Roshan-Sw <roshan.sam@spiderworks.in>
2025-05-23 16:27:15 +0530

    update

--- controllers/dummy.js
+++ controllers/dummy.js
@@ -11,20 +11,24 @@ export const createDummy = async (req, res) => {
     return res.status(201).json(newDummy);
   } catch (error) {
     console.error("Error creating dummy:", error);
-    return res.status(500).json({ error: "Failed to create dummy" });
+    return res.status(500).json({ error: "Failed to create dummy",message:error.message });
   }
 };
 
 // Get all dummy entries
 export const getAllDummies = async (req, res) => {
   try {
+    console.log('First Log');
+
     const dummies = await prisma.dummy.findMany({
       orderBy: { id: "desc" },
     });
 
+    console.log('Second Log');
+    
     return res.status(200).json(dummies);
   } catch (error) {
-    console.error("Error fetching dummies:", error);
-    return res.status(500).json({ error: "Failed to fetch dummies" });
+    console.error("Error fetching dummies:", );
+    return res.status(500).json({ error: error?.message });
   }
 };
