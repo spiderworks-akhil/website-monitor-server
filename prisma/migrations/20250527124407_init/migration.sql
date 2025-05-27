aee25d998f6332db74268bf2fdd684417f940929
Roshan-Sw <roshan.sam@spiderworks.in>
2025-05-27 18:15:45 +0530

    updates

---
+++ prisma/migrations/20250527124407_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `created_at` on the `CronFrequency` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CronFrequency" DROP COLUMN "created_at";
